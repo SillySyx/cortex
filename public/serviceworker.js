572d6fdef3b383820e0260cb17e9e434e0ec726f
Kristoffer Hägelstam <kristoffer.hagelstam@outlook.com>
2021-04-29 11:10:23 +0200

    updated service worker to handle updates better

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,52 +1,35 @@
-const version = '0.3';
-const ignoreFiles = [
-    "serviceworker.js",
-];
+const cacheName = "0.4";
 
-async function cache_request(request) {
-    const cached = await caches.match(request);
-    if (cached) {
-        return cached;
-    }
-
-    const response = await fetch(request);
-
-    const cache = await caches.open(`data-${version}`);
-    cache.put(request, response.clone());
-
-    return response;
-}
-
-function should_use_caching(request) {
-    if (self.location.hostname === "localhost" || self.location.hostname === "127.0.0.1")
-        return false;
-
-    if (ignoreFiles.some(file => request.url.endsWith(file)))
-        return false;
-
-    if (request.method !== "GET")
-        return false;
-
-    return true;
-}
-
-async function remove_old_caches() {
-    const keys = await caches.keys();
-
-    for (const key of keys) {
-        if (key !== `data-${version}`) {
-            caches.delete(key);
-        }
-    }
-}
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
 
 self.addEventListener("fetch", event => {
-    if (should_use_caching(event.request)) {
-        event.respondWith(cache_request(event.request));
-    }
+    if (event.request.method !== "GET")
+        return;
+
+    if (self.location.hostname === "localhost")
+        return;
+
+    event.respondWith((async () => {
+        let response = await caches.match(event.request);
+        if (response)
+            return response;
+
+        response = await fetch(event.request);
+        const cache = await caches.open(cacheName);
+        cache.put(event.request, response.clone());
+        return response;
+    })());
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(remove_old_caches());
+self.addEventListener("activate", event => {
+    event.waitUntil(caches.keys().then(keys => {
+        Promise.all(keys.map(key => {
+            if (key === cacheName) 
+                return;
+
+            caches.delete(key);
+        }))
+    }));
 });
-  
